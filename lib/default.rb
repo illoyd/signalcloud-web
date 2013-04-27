a0f78310de4e1238fce4fb62f4bee4224253981e
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-04-27 21:55:05 +0100

    Content updates

--- lib/default.rb
+++ lib/default.rb
@@ -5,3 +5,10 @@ gem 'haml'
 gem 'maruku'
 
 include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Rendering
+
+module ToolKitHelper
+  def logo
+    "<span class='logo'><i class='icon-cloud'></i><i class=icon-bolt'></i></span>"
+  end
+end
