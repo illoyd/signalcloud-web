261a1c8e675b7e1baf6d0271000d43c76b794339
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-06-30 12:06:44 +0100

    Huuuuuuuge updates

--- lib/default.rb
+++ lib/default.rb
@@ -8,7 +8,15 @@ include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 
 module ToolKitHelper
-  def logo
+  def self.logo
     "<span class='logo'><i class='icon-cloud'></i><i class=icon-bolt'></i></span>"
+  end
+  
+  def self.icon_stack( base, top )
+    "<span class='icon-stack'><i class='icon-#{base} icon-stack-base'></i><i class='icon-#{top} icon-stack-top'></i></span>"
+  end
+  
+  def self.number_icon( number, base='circle' )
+    "<span class='icon-stack'><i class='icon-#{base} icon-stack-base'></i><i class='icon-stack-top'>#{number}</i></span>"
   end
 end
